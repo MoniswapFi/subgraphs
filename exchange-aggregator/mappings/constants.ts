1a560211b50f0232b9803ec6330b5894176be9f8
Kingsley Victor <javaprodigy56@gmail.com>
2023-12-09 22:02:03 +0100

    made updates

--- exchange-aggregator/mappings/constants.ts
+++ exchange-aggregator/mappings/constants.ts
@@ -6,9 +6,13 @@ export const USDT = new TypedMap<string, string>();
 export const WETH = new TypedMap<string, string>();
 
 ROUTER_ADDRESS.set("goerli", "0xd6351CC74A04F9472dFBA0b5601d5Bb0d93F4E22");
+ROUTER_ADDRESS.set("mainnet", "0x8160C59218be97F301a857cD8E72e5d3446621df");
 WETH.set("goerli", "0x4200000000000000000000000000000000000006");
+WETH.set("mainnet", "0x4200000000000000000000000000000000000006");
 USDC.set("goerli", "0x2e9F75DF8839ff192Da27e977CD154FD1EAE03cf");
+USDC.set("mainnet", "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913");
 USDT.set("goerli", "0x3e8B7c72f4a9f4C8ec375c11F44FB84242c3893F");
+USDT.set("mainnet", "0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca");
 
 export const ZERO_BI = BigInt.zero();
 export const ZERO_BD = BigDecimal.zero();
