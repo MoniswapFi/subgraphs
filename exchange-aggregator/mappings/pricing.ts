bdad32df9e27aa9a435271404a4acd5cf88a1c9d
Kingsley Victor <javaprodigy56@gmail.com>
2023-12-10 15:24:32 +0100

    made updates

--- exchange-aggregator/mappings/pricing.ts
+++ exchange-aggregator/mappings/pricing.ts
@@ -1,4 +1,4 @@
-import { Address, BigDecimal, BigInt, dataSource, log } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt, dataSource } from "@graphprotocol/graph-ts";
 import { queryAmountOut } from "./utils/adapter";
 import { USDC, USDT, WETH, ZERO_BD, ZERO_BI } from "./constants";
 import { fetchTokenDecimals } from "./utils/erc20";
@@ -19,15 +19,33 @@ function getETHPriceInUSDFromAdapterQuery(address: Address): BigDecimal {
   );
   let price: BigDecimal = ZERO_BD;
 
-  if (WETH_USDT !== null && WETH_USDC !== null) {
-    price = WETH_USDT.plus(WETH_USDC).toBigDecimal();
+  if (WETH_USDT !== null) {
+    const USDT_DECIMALS = fetchTokenDecimals(Address.fromString(USDT.get(network) as string)) as BigInt;
+    price = WETH_USDT.toBigDecimal().div(
+      BigInt.fromI32(10)
+        .pow(USDT_DECIMALS.toI32() as u8)
+        .toBigDecimal(),
+    );
+  }
 
+  if (WETH_USDC !== null) {
+    const USDC_DECIMALS = fetchTokenDecimals(Address.fromString(USDC.get(network) as string)) as BigInt;
+    price = price.plus(
+      WETH_USDC.toBigDecimal().div(
+        BigInt.fromI32(10)
+          .pow(USDC_DECIMALS.toI32() as u8)
+          .toBigDecimal(),
+      ),
+    );
+  }
+
+  if (WETH_USDT !== null && WETH_USDC !== null) {
     if (WETH_USDT.gt(ZERO_BI) && WETH_USDC.gt(ZERO_BI)) {
       price = price.div(BigDecimal.fromString("2.0"));
     }
   }
 
-  return price.div(BigInt.fromU64(1e18 as u64).toBigDecimal());
+  return price;
 }
 
 export function getTokenPriceInUSDFromAdapterQuery(adapter: Address, token: Address): BigDecimal {
