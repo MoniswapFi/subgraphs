5aa9eb8c346d474d09fc9097bcdc75f38ba64478
Kingsley Victor <javaprodigy56@gmail.com>
2025-02-09 17:02:06 +0100

    deployed to berachain mainnet

--- voting/mappings/constants.ts
+++ voting/mappings/constants.ts
@@ -4,8 +4,10 @@ export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const VOTER_FACTORY = new TypedMap<string, string>();
 
 const BERA_TESTNET = "berachain-bartio";
+const BERA_MAINNET = "berachain-mainnet";
 
 VOTER_FACTORY.set(BERA_TESTNET, "0x6399081084Aff4A927E8704E77bC939703bd892c");
+VOTER_FACTORY.set(BERA_MAINNET, "0x8160C59218be97F301a857cD8E72e5d3446621df");
 
 export const BI_ZERO = BigInt.zero();
 export const BI_ONE = BigInt.fromU32(1);
